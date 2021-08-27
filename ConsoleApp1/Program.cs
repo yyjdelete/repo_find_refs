649d8feec9dbb76ea994021af7cf17e38b7a2737
SilverFox <yyjdelete@126.com>
2021-08-27 19:52:23 +0800

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using DotNetty.Common.Concurrency;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static IEventExecutor b;
+        static void Test()
+        {
+            b.SubmitAsync(() => 1);
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
