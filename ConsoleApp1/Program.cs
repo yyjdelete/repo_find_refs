a316d386a0a60fbf94fae56f917f2ade4ce3fc2a
SilverFox <yyjdelete@126.com>
2021-09-01 11:34:27 +0800

    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,16 +1,17 @@
-﻿using DotNetty.Common.Concurrency;
+﻿using System;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static IEventExecutor b;
-        static void Usage2()
-        {
-            b.SubmitAsync(() => 2);
-        }
+        static Microsoft.Extensions.Logging.ILogger logger;
         static void Main(string[] args)
         {
+#if NET5_0
+            logger.IsEnabled(Microsoft.Extensions.Logging.LogLevel.Information);
+#else
+            logger.IsEnabled(Microsoft.Extensions.Logging.LogLevel.Debug);
+#endif
         }
     }
 }
